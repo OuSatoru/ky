f0b5a29121150d59b080b12ffe38fbad596b96b2
Wang Cong <ousatoru@hotmail.com>
2018-12-03 17:01:44 +0800

    aaa

---
+++ godt/binarytree/bitree.go
@@ -0,0 +1,16 @@
+package main
+
+type Node struct {
+	key   int
+	value byte
+	left  *Node
+	right *Node
+}
+
+func main() {
+
+}
+
+func insert(node, newnode *Node) {
+	n := &Node(key, value, nil, nil)
+}
